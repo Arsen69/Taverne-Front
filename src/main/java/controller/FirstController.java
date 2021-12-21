7f0aff38c0ec4112f89dcfda357636a58a8c23f5
Antoine SPANIER <antoine.spanier@gmail.com>
2021-12-21 12:18:15 +0100

    hello world added

--- src/main/java/controller/FirstController.java
+++ src/main/java/controller/FirstController.java
@@ -1,16 +1,19 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
-	
+
 	public FirstController() {
 	}
-	
+
 	@RequestMapping("/hello")
-	public String hello() {
+	public String hello(@RequestParam(name = "prenom", required = false, defaultValue = "world") String prenom, Model model) {
+		model.addAttribute("prenom",prenom);
 		return "hello";
 	}
 
--- src/main/webapp/WEB-INF/views/hello.jsp
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>Test</h1>
+<h1>Hello ${prenom}</h1>
 </body>
 </html>
