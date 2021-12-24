610abd699428d9f2744c09d6ac604a5834087e60
Antoine SPANIER <antoine.spanier@gmail.com>
2021-12-24 13:04:07 +0100

    Avancement StockRestController

--- src/main/java/restcontroller/StockRestController.java
+++ src/main/java/restcontroller/StockRestController.java
@@ -2,9 +2,14 @@ package restcontroller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,9 +33,24 @@ public class StockRestController {
 
 	@GetMapping("/{id}/stocks")
 	@JsonView(JsonViews.Common.class)
-	public List<Stock> getAll(@PathVariable Long id) {
+	public List<Stock> getAllStocks(@PathVariable Long id) {
 		return stockService.getAllByBar(id);
 	}
-	
+
+	@GetMapping("/{id}/stocks/{idStock}")
+	@JsonView(JsonViews.Common.class)
+	public Stock getStockByIdStock(@PathVariable("id") Long idBar, @PathVariable Long idStock) {
+		return stockService.getByIdStockAndBar(idStock, idBar);
+	}
+
+	@PutMapping("/{id}/stocks/{idStock}")
+	@JsonView(JsonViews.Common.class)
+	public Stock updateLimit(@Valid @RequestBody Stock stock, BindingResult br, @PathVariable("id") Long idBar,
+			@PathVariable Long idStock) {
+		if (stock.getIdStock() == null) {
+			stock.setIdStock(idStock);
+		}
+		return stockService.getByIdStockAndBar(idStock, idBar);
+	}
 
 }
