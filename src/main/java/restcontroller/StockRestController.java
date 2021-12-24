bf173e603532732369795776c36099d536cc7f3c
Antoine SPANIER <antoine.spanier@gmail.com>
2021-12-24 16:13:53 +0100

    StockRestController fini + avancement ArticleRestController

--- src/main/java/restcontroller/StockRestController.java
+++ src/main/java/restcontroller/StockRestController.java
@@ -5,19 +5,27 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
+import exception.ArticleException;
 import model.JsonViews;
+import model.inventaire.Article;
 import model.inventaire.Bar;
 import model.inventaire.Stock;
+import service.ArticleService;
 import service.BarService;
 import service.StockService;
 
@@ -27,7 +35,8 @@ public class StockRestController {
 
 	@Autowired
 	private StockService stockService;
-
+	@Autowired
+	private ArticleService articleService;
 	@Autowired
 	private BarService barService;
 
@@ -51,6 +60,12 @@ public class StockRestController {
 			stock.setIdStock(idStock);
 		}
 		return stockService.getByIdStockAndBar(idStock, idBar);
+	}
+
+	@DeleteMapping("/{id}/stocks/{idStock}")
+	@JsonView(JsonViews.Common.class)
+	public void delete(@PathVariable Long id, @PathVariable Long idStock) {
+		stockService.delete(idStock);
 	}
 
 }
